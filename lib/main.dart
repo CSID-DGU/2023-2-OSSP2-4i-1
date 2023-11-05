84d5cb3dbaa70afcbebc5694048b6a499fc2b7b5
puretension <rlrlfhtm5@gmail.com>
2023-11-06 02:24:08 +0900

    branch test

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: Size(375, 812),
+      designSize: Size(376, 812),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
