1db78233bdaca6b5a1330875a71867ebeeeeecb1
bakyoungha <younghabak@gmail.com>
2023-11-07 03:52:13 +0900

    modify likes/models.py, migrate

--- backend/likes/models.py
+++ backend/likes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from users.models import User
+from pills.models import Pill
 
-# Create your models here.
+
+class Like(models.Model):
+    id = models.AutoField
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    pill_id = models.ForeignKey(Pill, on_delete=models.CASCADE)
