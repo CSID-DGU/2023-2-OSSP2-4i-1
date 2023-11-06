c5bdb201c067ccc285c97025513b750c664d87f6
bakyoungha <younghabak@gmail.com>
2023-11-07 03:21:06 +0900

    api 명세에 따라 app 분할

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
-
-class User(models.Model):
-    id = models.CharField(max_length=255, primary_key=True)
-    password = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    refresh_token = models.CharField(max_length=255)
+# Create your models here.
