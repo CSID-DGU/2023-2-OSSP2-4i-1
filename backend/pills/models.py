e176348e1acd5e94931928db84a55206aa3017a6
bakyoungha <younghabak@gmail.com>
2023-11-07 03:39:46 +0900

    modify pills/models.py, migrate

--- backend/pills/models.py
+++ backend/pills/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Pill(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    password = models.CharField(max_length=255)
+
+
+class PillProperties(models.Model):
+    id = models.ForeignKey(Pill, on_delete=models.CASCADE, primary_key=True)
+    custom_shape = models.CharField(max_length=255, null=True)
+    shape = models.CharField(max_length=255, null=True)
+    color1 = models.CharField(max_length=255, null=True)
+    color2 = models.CharField(max_length=255, null=True)
+    print_front = models.CharField(max_length=255, null=True)
+    print_back = models.CharField(max_length=255, null=True)
+    line_front = models.CharField(max_length=255, null=True)
+    line_back = models.CharField(max_length=255, null=True)
