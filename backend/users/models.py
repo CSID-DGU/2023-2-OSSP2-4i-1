82e883e51f33e6839bee4a70cb85e41536402c9e
bakyoungha <younghabak@gmail.com>
2023-11-07 03:27:36 +0900

    modify users/models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    password = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    refresh_token = models.CharField(max_length=255)
