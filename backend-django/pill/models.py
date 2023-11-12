4591d26903d8ca32291d04b934c92fe464e60625
bakyoungha <younghabak@gmail.com>
2023-11-12 22:13:28 +0900

    약 모델 정의, 마이그레이션 생성 및 적용까지 완료

--- backend-django/pill/models.py
+++ backend-django/pill/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Pill(models.Model):
+    name = models.CharField(max_length=255)
+    img_link = models.CharField(max_length=255)
+    label_forms = models.CharField(max_length=255, blank=True)
+    label_shapes = models.CharField(max_length=255, blank=True)
+    label_color1 = models.CharField(max_length=255, blank=True)
+    label_color2 = models.CharField(max_length=255, blank=True)
+    label_line_front = models.CharField(max_length=255, blank=True)
+    label_line_back = models.CharField(max_length=255, blank=True)
+    label_print_front = models.CharField(max_length=255, blank=True)
+    label_print_back = models.CharField(max_length=255, blank=True)
