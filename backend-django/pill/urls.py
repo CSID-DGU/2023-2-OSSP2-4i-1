fa511f201a8c83e893b83e008936fc63626ea412
bakyoungha <younghabak@gmail.com>
2023-11-15 15:04:56 +0900

    텍스트로 검색 url 매핑 설정 수정

--- backend-django/pill/urls.py
+++ backend-django/pill/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('search', TextSearchAPIView.as_view()),
+    path('search/', TextSearchAPIView.as_view()),
     path('search/image', SearchAPIView.as_view()),
     path('<int:pill_id>', IDSearchAPIView.as_view()),
     path('<int:pill_id>/likes', LikeAPIView.as_view()),
