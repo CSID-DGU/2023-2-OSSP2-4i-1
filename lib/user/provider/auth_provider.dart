a34560d709f2bddbc9084046c7f0d397e0d9d49e
puretension <rlrlfhtm5@gmail.com>
2023-11-21 10:38:46 +0900

    라우터에 detailScreen부분 반영

--- lib/user/provider/auth_provider.dart
+++ lib/user/provider/auth_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:yakmoya/camera_ex.dart';
+import 'package:yakmoya/image_search_screen.dart';
 import 'package:yakmoya/pill/pill_picture/view/search_detail_screen.dart';
 import 'package:yakmoya/pill/pill_picture/view/text_search_screen.dart';
 import 'package:yakmoya/user/model/user_model.dart';
