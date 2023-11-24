6e908c8386afb4a2227183832b1a3a0006d9451b
bakyoungha <younghabak@gmail.com>
2023-11-24 18:58:51 +0900

    성분표, 상호작용표 두개의 테이블로 분리 / 마이그레이션

--- backend-django/pill/models.py
+++ backend-django/pill/models.py
@@ -26,9 +26,14 @@ class Instructions(models.Model):
     pill_inter = models.CharField(max_length=2555, blank=True)
 
 
+class PillComponent(models.Model):
+    pill_name = models.CharField(max_length=255)
+    pill_component = models.CharField(max_length=255)
+
+
 class Interactions(models.Model):
-    name1 = models.CharField(max_length=255)
-    name2 = models.CharField(max_length=255)
+    component_name1 = models.CharField(max_length=255)
+    component_name2 = models.CharField(max_length=255)
     clinical_effect = models.CharField(max_length=255)
     mechanism = models.CharField(max_length=255)
     handle = models.CharField(max_length=255)
