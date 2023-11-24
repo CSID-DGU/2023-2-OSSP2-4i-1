dffc0ed9d3188ff2ad9d2484e88dc71b1fb32307
bakyoungha <younghabak@gmail.com>
2023-11-24 15:07:45 +0900

    url 매핑 수정

--- backend-django/pill/urls.py
+++ backend-django/pill/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('search/', TextSearchAPIView.as_view()),
     path('search/image', SearchAPIView.as_view()),
     path('<int:pill_id>', IDSearchAPIView.as_view()),
-    path('<int:pill_id>/likes', LikeAPIView.as_view()),
+    path('likes/<int:pill_id>', LikeAPIView.as_view()),
 ]
