823d5414c7a3ab54f49a354be99ee0836c170b59
bakyoungha <younghabak@gmail.com>
2023-11-29 02:36:14 +0900

    기본검색(텍스트/이미지속성)시 아이디,약이름,이미지링크만 반환하도록 수정

--- backend-django/pill/serializers.py
+++ backend-django/pill/serializers.py
@@ -6,7 +6,9 @@ from pill.models import Pill, Instructions
 class PillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pill
-        fields = '__all__'
+        fields = [
+            'id', 'name', 'img_link'
+        ]
 
 
 class InstructionsSerializer(serializers.ModelSerializer):
