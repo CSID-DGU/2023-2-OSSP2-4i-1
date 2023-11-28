960f469002e09450fcfdd2def73beecb38f3c20b
puretension <rlrlfhtm5@gmail.com>
2023-11-28 21:24:54 +0900

    기존 홈화면에서 UI UX를 고려한 새로운 홈화면 구현

--- lib/user/view/home_screen.dart
+++ lib/user/view/home_screen.dart
@@ -1,352 +1,229 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:yakmoya/common/component/login_next_button.dart';
+import 'package:yakmoya/common/const/tab_item.dart';
 import 'package:yakmoya/image_search_screen.dart';
 import 'package:yakmoya/common/const/colors.dart';
 import 'package:yakmoya/pill/pill_picture/view/text_search_screen.dart';
+import 'package:yakmoya/user/model/user_model.dart';
+import 'package:yakmoya/user/provider/user_me_provider.dart';
+import 'package:yakmoya/user/view/splash_screen.dart';
 
-// 여기서 Dashboard, Chat, Profile, Settings 위젯을 정의해야 합니다.
-// 예를 들어 간단한 컨테이너로 대체할 수 있습니다.
-class Dashboard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(child: Text('Dashboard'));
-  }
-}
+import 'alarm_screen.dart';
 
-class Chat extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(child: Text('Chat'));
-  }
-}
-
-class Profile extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(child: Text('Profile'));
-  }
-}
-
-class Settings extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(child: Text('Settings'));
-  }
-}
-
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   static String get routeName => 'home';
+
+  final TabController? tabController;
+
+  HomeScreen({this.tabController});
+
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-
-  final PageStorageBucket bucket = PageStorageBucket();
-
-  void _showDialog(String title) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(title),
-          content: Text('이곳에서 $title 기능을 설정할 수 있습니다.'),
-          actions: <Widget>[
-            TextButton(
-              child: Text('닫기'),
-              onPressed: () {
-                Navigator.of(context).pop();
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    final userState = ref.watch(userMeProvider);
+    UserModel? user;
+    if (userState is UserModel) {
+      user = userState; // UserModel로 캐스팅
+    }
+    // user 또는 userPointState가 로딩 중일 때 로딩 인디케이터를 표시
+    if (user == null) {
+      print('로딩중...');
+      return const SplashScreen();
+    }
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SvgPicture.asset(
+              'assets/img/homelogo.svg',
+            ),
+            InkWell(
+              child: SvgPicture.asset(
+                'assets/img/alarm.svg',
+                width: 40,
+                height: 40,
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AlarmScreen(),
+                  ),
+                );
               },
             ),
+            SizedBox(
+              width: 30,
+            ),
+            SvgPicture.asset(
+              'assets/img/setting.svg',
+              width: 40,
+              height: 40,
+            ),
           ],
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // 이 코드는 FilterNetworkListScreen을 열고 결과를 기다립니다.
-    // void _openFilterNetworkListScreen() async {
-    //   final List<Pill> selectedPills = await Navigator.of(context).push(
-    //     MaterialPageRoute(
-    //       builder: (context) => FilterNetworkListScreen(),
-    //     ),
-    //   );
-    //
-    //   if (selectedPills != null && selectedPills.isNotEmpty) {
-    //     setState(() {
-    //       // TODO: 여기서 selectedPills 리스트의 내용을 HomeScreen의 products 리스트에 추가합니다.
-    //       // 예를 들어, products.addAll(selectedPills.map((pill) => HomePill(pill.assetName, pill.name)).toList());
-    //       // 이렇게 하면, Pill 객체들이 HomePill 객체로 변환되어 products 리스트에 추가됩니다.
-    //     });
-    //   }
-    // }
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: PRIMARY_BLUE_COLOR,
-        title: const Text(
-          '도형님',
-          style: TextStyle(fontWeight: FontWeight.w700),
         ),
-        centerTitle: true,
-        // Use a Row widget to have better control over the alignment of the children.
-        leading: GestureDetector(
-          onTap: () {
-            // TODO: Navigate to notifications screen
-          },
-          child: Image.asset(
-            'assets/img/alarm.png',
-            width: 24,
-            height: 24,
-          ),
-        ),
-        actions: [
-          GestureDetector(
-            onTap: () {
-              // TODO: Navigate to settings screen
-            },
-            child: Padding(
-              padding: const EdgeInsets.only(right: 10),
-              child:
-                  Image.asset('assets/img/setting.png', width: 44, height: 44),
-            ),
-          ),
-        ],
       ),
-      backgroundColor: PRIMARY_BLUE_COLOR,
-      body: Column(
-        children: [
-          Expanded(
-            // PageStorage를 사용하여 GridView.builder의 상태를 저장합니다.
-            child: PageStorage(
-              bucket: bucket,
-              child: GridView.builder(
-                key: PageStorageKey<String>('gridView'),
-                padding: const EdgeInsets.fromLTRB(
-                    10, 100, 10, 10), // You can increase padding if needed
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 1,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 16,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(
+          10,
+          10,
+          10,
+          10,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '안녕하세요.\n${user.name}님',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 26,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Container(
+                width: 370,
+                height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1.0,
+                    color: BLUE_BLUE_COLOR,
+                  ),
+                  color: BLUE_BLUE_COLOR,
+                  borderRadius: BorderRadius.circular(7.0),
                 ),
-                itemCount: products.length,
-                itemBuilder: (context, index) =>
-                    _buildProductItem(context, products[index]),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          height: 18,
+                          child: Text(
+                            '내 약통',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: PRIMARY_BLUE_COLOR,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 40,
+                          child: Text(
+                            '알약을 추가하여 상호작용 여부를 확인해 보세요!',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey[600],
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      ), // GridView.builder(gridDelegate: gridDelegate, itemBuilder: itemBuilder)
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: Container(
-        height: 100.0, // 원하는 높이 설정
-        width: 100.0, // 원하는 너비 설정
-        child: FittedBox(
-          child: FloatingActionButton(
-            child: Text(
-              '검사\n하기',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16), // 텍스트 크기를 조정하여 적절하게 맞춤
-            ),
-            onPressed: () {
-              // 버튼을 눌렀을 때의 액션
-            },
-            backgroundColor: Colors.red[300],
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(30.0),
-              ),
-            ),
-          ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: InkWell(
-                onTap: () => _showDialog('Dashboard'),
-                child: const Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.star, color: Colors.blue, size: 30),
-                    Text(
-                      '복용중',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15), // 모서리를 원형으로 깎음
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: GridView.builder(
+                          shrinkWrap: true, // GridView를 Column 내부에 넣을 때 필요
+                          physics: NeverScrollableScrollPhysics(), // 스크롤 동작 비활성화
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4, // 한 줄에 4개의 아이템
+                            childAspectRatio: 1, // 아이템의 가로세로 비율
+                            crossAxisSpacing: 10, // 수평 간격
+                            mainAxisSpacing: 10, // 수직 간격
+                          ),
+                          itemCount: 8, // 총 8개의 아이템
+                          itemBuilder: (context, index) {
+                            // 첫 번째 아이템인 경우
+                            if (index == 0) {
+                              return GestureDetector(
+                                onTap: () {
+                                  // showDialog 구현
+                                },
+                                child: Column(
+                                  children: [
+                                    ClipOval(
+                                      child: SvgPicture.asset(
+                                        'assets/img/plus.svg',
+                                        width: 60,
+                                        height: 60,
+                                      ),
+                                    ),
+                                    Text('추가하기',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                            fontSize: 15,
+                                            fontWeight:
+                                                FontWeight.w700)), // 텍스트 크기 조정
+                                  ],
+                                ),
+                              );
+                            }
+                            // 나머지 아이템들
+                            return Column(
+                              children: [
+                                ClipOval(
+                                  child: SvgPicture.asset(
+                                    'assets/img/plus.svg', // 추후 변경 가능
+                                    width: 60,
+                                    height: 60,
+                                  ),
+                                ),
+                                Text('알약${index}',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w700,
+                                      color: Colors.grey[500]
+                                    )), // 텍스트 크기 조정
+                              ],
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: LoginNextButton(
+                        onPressed: () {},
+                        buttonName: '검사하기',
+                        isButtonEnabled: true,
+                        color: PRIMARY_BLUE_COLOR,
                       ),
                     ),
                   ],
                 ),
               ),
             ),
-            Expanded(
-              child: InkWell(
-                onTap: () => _showDialog('Settings'),
-                child: const Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.search, color: Colors.grey, size: 30),
-                    Text(
-                      '검색하기',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
           ],
         ),
       ),
     );
   }
-}
-
-class HomePill {
-  final String imagePath;
-  final String name; // 이름을 저장할 필드를 추가합니다.
-
-  const HomePill(this.imagePath, this.name);
-
-  Widget get imageWidget {
-    return Image.asset(imagePath);
-  }
-}
-
-final List<HomePill> products = [
-  HomePill('assets/img/p1.png', '알약1'),
-  HomePill('assets/img/p2.png', '알약2'),
-  HomePill('assets/img/plus.png', '추가하기'),
-  HomePill('assets/img/warning.png', ''),
-];
-
-Widget _buildProductItem(BuildContext context, HomePill pill) {
-  // '추가하기' 선택 시 표시될 대화상자
-  void _showAddDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12.0), // 모서리를 둥글게
-            side: BorderSide(
-              color: PRIMARY_BLUE_COLOR,
-              width: 2,
-            ), // 테두리 색과 두께
-          ),
-          backgroundColor: Colors.white,
-          title: Center(
-            child: const Text('약 추가하기'),
-          ),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Divider(
-                color: Colors.grey[300],
-                thickness: 2,
-              ),
-              buildElevatedButton('이름으로 추가📝', () {
-                Navigator.of(context).pop(); // 대화상자 닫기
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const TextSearchScreen(),
-                  ),
-                );
-              }),
-              buildElevatedButton('사진으로 추가📷', () {
-                Navigator.of(context).pop(); // 대화상자 닫기
-                // TODO: 사진으로 선택 화면으로 이동
-                // Navigator.of(context).push(
-                //   MaterialPageRoute(
-                //     builder: (context) => CameraExample(),
-                //   ),
-                // );
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const ImageSearchScreen(),
-                  ),
-                );
-              }),
-              buildElevatedButton('복용중에서 추가💊', () {
-                Navigator.of(context).pop(); // 대화상자 닫기
-                // TODO: 복용중에서 선택 화면으로 이동
-              }),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  return InkWell(
-    onTap: () {
-      // '추가하기'를 위한 대화상자 표시
-      if (pill.name == '추가하기') {
-        _showAddDialog();
-      } else {
-        // TODO: Navigate to detail screen
-      }
-    },
-    child: Column(
-      // Stretch the items to fill the Card
-      children: [
-        Expanded(
-          child: ClipRRect(
-            // Clip the image with rounded corners
-            borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
-            child: pill.imageWidget, // Pill image
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            pill.name,
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        // Add more info here if needed
-      ],
-    ),
-  );
-}
-
-Widget buildElevatedButton(String buttonName, VoidCallback onPressed) {
-  return Padding(
-    padding: const EdgeInsets.symmetric(vertical: 10),
-    child: ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        foregroundColor: Colors.black, // 글자 색상
-        backgroundColor: Colors.white, // 배경 색상
-        side: const BorderSide(
-          color: PRIMARY_BLUE_COLOR, // 여기에 실제 사용할 색상 코드를 입력하세요.
-        ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8), // 테두리 둥글기
-        ),
-        minimumSize: const Size(
-          double.infinity, // 버튼 너비 최대로
-          50, // 버튼 높이
-        ),
-      ),
-      child: Text(
-        buttonName,
-        style: const TextStyle(
-            fontWeight: FontWeight.w900, fontSize: 22, color: BLUE_COLOR),
-      ),
-    ),
-  );
 }
