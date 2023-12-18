951119355f85ac061b0623cab43f34be3a045782
puretension <rlrlfhtm5@gmail.com>
2023-12-19 07:27:09 +0900

    ⏰알림 스크린, 기능 구현

--- lib/user/view/alarm_screen.dart
+++ lib/user/view/alarm_screen.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:yakmoya/common/component/custom_dropdown_button.dart';
 import 'package:yakmoya/common/const/colors.dart';
 import 'package:yakmoya/common/view/default_layout.dart';
+import 'package:yakmoya/user/provider/user_me_provider.dart';
 import 'package:yakmoya/user/view/pill_card.dart';
+import 'package:yakmoya/user/view/splash_screen.dart';
+
+import '../../common/component/custom_text_form_field.dart';
+import '../../common/component/login_next_button.dart';
+import '../../common/const/text.dart';
+import '../model/user_model.dart';
+import '../model/user_pill_model.dart';
 
 class AlarmScreen extends ConsumerStatefulWidget {
   const AlarmScreen({super.key});
@@ -15,18 +25,41 @@ class AlarmScreen extends ConsumerStatefulWidget {
 class _AlarmScreenState extends ConsumerState<AlarmScreen> {
   @override
   Widget build(BuildContext context) {
+    final userState = ref.watch(userMeProvider);
+    final pillsState = ref.watch(pillsProvider);
+
+    UserModel? user;
+    List<UserPillModel>? pills;
+
+    // User 상태 확인
+    if (userState is UserModel) {
+      user = userState;
+    }
+
+    // Pills 상태 확인
+    if (pillsState is AsyncData<List<UserPillModel>>) {
+      pills = pillsState.value;
+    }
+
+    // 로딩 상태 또는 에러 상태일 때 처리
+    if (pills == null || user == null) {
+      return SplashScreen();
+    }
+
     return DefaultLayout(
       backgroundColor: Colors.white,
       title: '약 알림 목록',
       child: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: samplePills.length,
-              itemBuilder: (context, index) {
-                return PillCard(samplePills[index]);
-              },
-            ),
+            child: pills != null && pills.isNotEmpty
+                ? ListView.builder(
+                    itemCount: pills.length,
+                    itemBuilder: (context, index) {
+                      return PillCard(pills![index],context);
+                    },
+                  )
+                : Center(child: Text('알약이 없습니다')), // pills가 null이거나 비어있을 경우
           ),
         ],
       ),
@@ -34,201 +67,187 @@ class _AlarmScreenState extends ConsumerState<AlarmScreen> {
   }
 }
 
-final samplePills = [
-  SamplePill(
-      name: '타이레놀정500mg',
-      num: 1,
-      morning: true,
-      lunch: true,
-      dinner: true,
-      imgUrl: 'assets/img/pills/alarmPill.svg'),
-  SamplePill(
-      name: '아스피린정500mg',
-      num: 2,
-      morning: true,
-      lunch: true,
-      dinner: true,
-      imgUrl: 'assets/img/pills/alarmPill.svg'),
-  SamplePill(
-      name: '가스피린정 500mg',
-      num: 3,
-      morning: true,
-      lunch: true,
-      dinner: true,
-      imgUrl: 'assets/img/pills/alarmPill.svg'),
-];
+Widget PillCard(UserPillModel pill, BuildContext context) {
+  String parsePillName(String name) {
+    final mgIndex = name.indexOf('mg');
+    if (mgIndex != -1) {
+      // 'mg'가 존재하면 'mg'를 포함하여 그 전까지의 문자열을 반환합니다.
+      return name.substring(0, mgIndex + 2);
+    } else {
+      // 'mg'가 없으면 첫 번째 공백 전까지의 문자열을 반환합니다.
+      final firstSpaceIndex = name.indexOf(' ');
+      if (firstSpaceIndex != -1) {
+        return name.substring(0, firstSpaceIndex);
+      }
+    }
+    // 위 조건에 해당하지 않으면 원본 문자열을 그대로 반환합니다.
+    return name;
+  }
 
-class SamplePill {
-  final String name;
-  final int num;
-  final bool morning;
-  final bool lunch;
-  final bool dinner;
-  final String imgUrl;
-
-  SamplePill({
-    required this.name,
-    required this.num,
-    required this.morning,
-    required this.lunch,
-    required this.dinner,
-    required this.imgUrl,
-  });
-}
-
-Widget PillCard(SamplePill pill) {
   return Padding(
     padding: const EdgeInsets.symmetric(vertical: 10),
     child: Card(
       color: Colors.white,
-      child: InkWell(
-        onTap: () {},
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Container(
-            width: 335,
-            height: 150,
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 1.0,
-                color: PRIMARY_BLUE_COLOR,
-              ),
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10.0),
+      child: Container(
+        width: 335,
+        height: 150,
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1.0,
+            color: PRIMARY_BLUE_COLOR,
+          ),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Row(
+          children: [
+            Image.network(
+              pill.img,
+              height: 100,
             ),
-            child: Row(
-              children: [
-                Image.asset('assets/pills/alarmPill.png'),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 22, 0, 0),
-                        child: Text(
-                          pill.name!,
-                          style: const TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w700,
-                          ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 5),
+                      child: Text(
+                        parsePillName(pill.name) ?? 'Unknown',
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w700,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                        child: Text(
-                          '남은 복용 알약 ${pill.num}',
-                          style: const TextStyle(
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(5),
-                            child: Container(
-                              width: 45,
-                              height: 21,
-                              decoration: BoxDecoration(
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Container(
+                            width: 45,
+                            height: 21,
+                            decoration: BoxDecoration(
+                              color: PRIMARY_BLUE_COLOR,
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(
                                 color: PRIMARY_BLUE_COLOR,
-                                borderRadius: BorderRadius.circular(8),
-                                border: Border.all(
-                                  color: PRIMARY_BLUE_COLOR,
-                                  width: 1,
-                                ),
+                                width: 1,
                               ),
-                              child: Center(
-                                child: Text(
-                                  '아침',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 14.0,
-                                    fontWeight: FontWeight.w800,
-                                  ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                '아침',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w800,
                                 ),
                               ),
                             ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(5),
-                            child: Container(
-                              width: 45,
-                              height: 21,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(8),
-                                border: Border.all(
-                                  color: PRIMARY_BLUE_COLOR,
-                                  width: 1,
-                                ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: Container(
+                            width: 45,
+                            height: 21,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(
+                                color: PRIMARY_BLUE_COLOR,
+                                width: 1,
                               ),
-                              child: Center(
-                                child: Text(
-                                  '점심',
-                                  style: TextStyle(
-                                    color: PRIMARY_BLUE_COLOR,
-                                    fontSize: 14.0,
-                                    fontWeight: FontWeight.w800,
-                                  ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                '점심',
+                                style: TextStyle(
+                                  color: PRIMARY_BLUE_COLOR,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w800,
                                 ),
                               ),
                             ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(5),
-                            child: Container(
-                              width: 45,
-                              height: 21,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(8),
-                                border: Border.all(
-                                  color: PRIMARY_BLUE_COLOR,
-                                  width: 1,
-                                ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: Container(
+                            width: 45,
+                            height: 21,
+                            decoration: BoxDecoration(
+                              color: PRIMARY_BLUE_COLOR,
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(
+                                color: PRIMARY_BLUE_COLOR,
+                                width: 1,
                               ),
-                              child: Center(
-                                child: Text(
-                                  '저녁',
-                                  style: TextStyle(
-                                    color: PRIMARY_BLUE_COLOR,
-                                    fontSize: 14.0,
-                                    fontWeight: FontWeight.w800,
-                                  ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                '저녁',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w800,
                                 ),
                               ),
                             ),
                           ),
-                        ],
-                      ),
-                      Row(
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 20, 0),
+                      child: Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
                           InkWell(
                             child: SvgPicture.asset(
                               'assets/img/pencil.svg',
-                              height: 25,
+                              height: 35,
                             ),
                           ),
                           InkWell(
                             child: SvgPicture.asset(
                               'assets/img/trash.svg',
-                              height: 25,
+                              height: 35,
                             ),
                           ),
                         ],
                       ),
-                    ],
-                  ),
-                )
-              ],
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
+          ],
         ),
       ),
     ),
   );
+}
+
+
+
+class SignupAskLabel extends StatelessWidget {
+  final String text;
+  const SignupAskLabel({required this.text, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 380,
+      height: 30,
+      child: Text(
+        text,
+        style: customTextStyle,
+        maxLines: 2,
+      ),
+    );
+  }
 }
